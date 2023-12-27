e286beaaaa0ab09c0f4fda38d5a672bdd87ee9b9
mdavletova <146645176+mdavletova@users.noreply.github.com>
2023-12-27 11:06:44 +0300

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class MedianFinder {
+    vector<int> counter; // вектор для хранения количества чисел в диапазоне [0, 100]
+    int totalNumbers; // Общее количество чисел
+
+public:
+    MedianFinder() : counter(101, 0), totalNumbers(0) {}
+
+    void addNum(int num) {
+        counter[num]++;
+        totalNumbers++;
+    }
+
+    double findMedian() {
+        int count = 0;
+        int median1 = -1, median2 = -1; // Для четного количества чисел
+
+        for (int i = 0; i <= 100; i++) {
+            count += counter[i];
+            if (median1 == -1 && count >= (totalNumbers + 1) / 2) {
+                median1 = i;
+            }
+            if (count >= (totalNumbers + 2) / 2) {
+                median2 = i;
+                break;
+            }
+        }
+
+        if (totalNumbers % 2 == 0) {
+            return (median1 + median2) / 2.0;
+        }
+        else {
+            return median2;
+        }
+    }
+};
+
+int main() {
+    string choice;
+    do {
+        MedianFinder mf;
+        int arr[] = { 1,2,3,4,5 };
+        int n = sizeof(arr) / sizeof(arr[0]);
+
+        for (int i = 0; i < n; i++) {
+            mf.addNum(arr[i]);
+        }
+
+        cout << "Median is: " << mf.findMedian() << std::endl;
+        cout << "Run again? (y/n): ";
+        cin >> choice;
+    } while (choice == "y" || choice == "Y");
+    return 0;
+}
---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+bool isSafe(vector<string>& board, int row, int col, int n) {
+    // Проверяем вертикали вверх
+    for (int i = 0; i < row; ++i) {
+        if (board[i][col] == 'Q') {
+            return false;
+        }
+    }
+
+    // Проверяем левую верхнюю диагональ
+    for (int i = row, j = col; i >= 0 && j >= 0; --i, --j) {
+        if (board[i][j] == 'Q') {
+            return false;
+        }
+    }
+
+    // Проверяем правую верхнюю диагональ
+    for (int i = row, j = col; i >= 0 && j < n; --i, ++j) {
+        if (board[i][j] == 'Q') {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void solveNQueens(int row, int n, vector<string>& board, vector<vector<string>>& result) {
+    if (row == n) {
+        result.push_back(board);
+        return;
+    }
+
+    for (int col = 0; col < n; ++col) {
+        if (isSafe(board, row, col, n)) {
+            board[row][col] = 'Q';
+            solveNQueens(row + 1, n, board, result);
+            board[row][col] = '.'; // Бектрек
+        }
+    }
+}
+
+vector<vector<string>> solveNQueens(int n) {
+    vector<vector<string>> result;
+    vector<string> board(n, string(n, '.'));
+
+    solveNQueens(0, n, board, result);
+
+    return result;
+}
+
+int main() {
+    string choice;
+    do {
+        int n = 4; // Измените значение n для других размеров доски
+        vector<vector<string>> solutions = solveNQueens(n);
+
+        cout << "Solutions for " << n << " queens on the board " << n << "x" << n << ":\n";
+        for (const auto& solution : solutions) {
+            for (const string& row : solution) {
+                cout << row << endl;
+            }
+            cout << "-----------------\n";
+            cout << "Run again? (y/n): ";
+            cin >> choice;
+        }
+    } while (choice == "y" || choice == "Y");
+    return 0;
+}
